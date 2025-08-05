f8a14a916cc3e4e5f112ba33c92c1353c665ea1a
João Baptista Mesquita <joaob.mesquita1@gmail.com>
2025-08-05 07:39:02 -0300

    configuração do tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/**
+ @type {import('tailwindcss').Config}
+ */
+
+ module.exports = {
+    content: [
+        "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte,mdx,md}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+ }
