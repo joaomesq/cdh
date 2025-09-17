688cb67b5052892a30563f0c14e39dd34b390e59
João Baptista Mesquita <joaob.mesquita1@gmail.com>
2025-09-17 18:19:18 -0300

    Reajustando o resumo para esconder missão e visão em telas maiores

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps{
 
 export default function Card({titulo, descricao, background = ''}: CardProps){
     return(
-        <div className={`rounded p-6 text-center m-2 ${background !== ''? background: "bg-cdh-blue"}`}>
+        <div className={`rounded p-4 text-center m-2 ${background !== ''? background: "bg-cdh-blue"}`}>
             <h4 className='font-bold mb-2'>{titulo}</h4>
             <p>{descricao}</p>
         </div>
