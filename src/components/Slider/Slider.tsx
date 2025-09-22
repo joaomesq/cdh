c3635ad5ca9602872c31da203d0f711fb02ba0eb
João Baptista Mesquita <joaob.mesquita1@gmail.com>
2025-09-22 17:09:52 -0300

    ajustando o posicionamento do slider para nao passar por cima

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -33,18 +33,17 @@ export default function Slider(){
     useEffect(()=> {
         const interval = setInterval(() => {
             setCurrent((prev) => (prev +1) % slides.length);
-        }, 2000);
+        }, 3000);
         return () => clearInterval(interval);
     }, []);
 
     const { imagem, titulo, descricao } = slides[current];
     return (
-        <section className="w-full h-[400px] flex items-center justify-center text-white text-center relative" style={{ backgroundImage: `url(${imagem})`, backgroundSize: "Cover", backgroundPosition: "center"}}>
+        <section className="w-full h-[400px] flex items-center flex-col justify-center text-white text-center" style={{ backgroundImage: `url(${imagem})`, backgroundSize: "Cover", backgroundPosition: "center"}}>
             {/*escurecendo o fundo*/}
-            <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
             {/*Textos*/}
-            <div className="relative max-w-xl text-left px-4">
+            <div className="max-w-xl text-left md:text-center p-4 py-8 h-[300px]">
                 <h2 className="text-3xl font-bold mb-2">{titulo}</h2>
                 <p className="text-lg mb-4">{descricao}</p>
 
@@ -53,9 +52,9 @@ export default function Slider(){
             </div>
 
             {/*Controllers*/}
-            <div className="absolute bottom-4 left-1/1 flex space-x-2 z-10">
+            <div className="">
                 {slides.map((_, index)=> (
-                    <button key={index} onClick={()=> setCurrent(index)} className={`w-3 h-3 rounded-full ${current === index ? "bg-white": "bg-gray-400"}`} />
+                    <button key={index} onClick={()=> setCurrent(index)} className={`w-3 h-3 m-2 rounded-full ${current === index ? "bg-white": "bg-gray-400"}`} />
                 ))}
             </div>
         </section>
