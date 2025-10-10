9d72220e318802657e1f6161f08be78e25a7c9e8
João Baptista Mesquita <joaob.mesquita1@gmail.com>
2025-10-10 20:02:41 -0300

    Escurecendo fundo do slide. Refatoração do código

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -34,42 +34,40 @@ export default function Slider(){
         const interval = setInterval(() => {
             setCurrent((prev) => (prev +1) % slides.length);
         }, 2500);
+        
         return () => clearInterval(interval);
     }, []);
 
     const { imagem, titulo, descricao } = slides[current];
+    
     return (
-        <>
-        <section className="w-full py-20 md:p-48 h-[700px] md:h-[700px] flex flex-col items-center justify-around text-black" 
-         style={{ backgroundImage: `url(${imagem})`, backgroundSize: "Cover", backgroundPosition: "center"}}
-        >
-            {/*escurecendo o fundo*/}
-        <main className="flex items-center h-[80%] justify-center  text-black text-center">
-            {/*Textos*/}
-            <div className="max-w-4xl p-4 py-3 flex flex-col items-center justify-center">
-                <h2 className="text-5xl text-orange-400 font-bold mb-2">{titulo}</h2>
-                <p className="text-lg text-white mb-4">{descricao}</p>
+        <section className="relative w-full h-[500px] py-6" style={{ backgroundImage: `url(${imagem})`, backgroundSize: "Cover", backgroundPosition: "center"}}>         
+            {/*Escurecendot o fundo */}
+            <div className="absolute inset-0 bg-black bg-opacity-60"></div>
+            
+            <div className="relative z-10">
+                <div className="flex items-center h-[80%] justify-center text-black text-center">
+                    {/*Textos*/}
+                    <div className="max-w-4xl p-4 py-3 flex flex-col items-center justify-center">
+                        <h2 className="text-5xl text-orange-400 font-bold mb-2">{titulo}</h2>
+                        <p className="text-lg text-white mb-4">{descricao}</p>
+                    </div>
+                </div>
+
+                <div className="flex items-center justify-center w-full ">
+                    {/*Controllers*/}
+                    <div>
+                        {slides.map((_, index)=> (
+                            <button key={index} onClick={()=> setCurrent(index)} className={`w-3 h-3 m-2 rounded-full ${current === index ? "bg-orange-600": "bg-orange-200"}`} />
+                        ))}
+                    </div>
+                </div>
+
+                <div className="flex items-center mb-4 h-[5%] justify-center mt-10 w-full ">
+                    <CallAction texto="Junte-se a nós" link="https://www.facebook.com/groups*sdh.community.group/permalink/3980161625462870"/>
+                </div>
 
             </div>
-       </main>
-
-  <main className="flex items-center h-[5%] justify-center w-full ">
-              {/*Call to action */}
-                <div className="">
-                {slides.map((_, index)=> (
-                    <button key={index} onClick={()=> setCurrent(index)} className={`w-3 h-3 m-2 rounded-full ${current === index ? "bg-orange-600": "bg-orange-200"}`} />
-                ))}
-            </div>
-          </main>
-
-      {/*Controllers*/}
-          <main className="flex items-center h-[5%] justify-center mt-10 w-full ">
-              <CallAction 
-                texto="Junte-se a nós" 
-                link="https://www.facebook.com/groups*sdh.community.group/permalink/3980161625462870"/>
-          </main>
-
         </section>
-       </>
     );
 }
