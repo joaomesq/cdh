0e39adf380dd5bb65701e59f9c5466b315aea45d
João Baptista Mesquita <joaob.mesquita1@gmail.com>
2025-10-11 09:22:11 -0300

    atualização do estilo newletter

--- src/components/Newsletter/Newsletter.tsx
+++ src/components/Newsletter/Newsletter.tsx
@@ -6,19 +6,19 @@ interface NewsletterProps{
 
 export default function Newsletter({email}: NewsletterProps){
     return (
-        <div className='bg-orange-300 shadow-lg rounded-xl p-8 max-w-4xl w-full text-center'>
-            <div>
-                <h3 className='text-2xl font-bold mb-4 text-white'>Assine nossa Newsletter</h3>
-                <p className='mb-6 text-white'>Receba novidades, dicas e conteúdos exclusivos no seu e-mail.</p>
+        <div className='bg-cdh-blue p-8 py-12 w-full text-center'>
+            <div className='md:w-1/2 md:mx-auto'>
+                <h3 className='text-3xl font-bold mb-4 text-white'>Assine nossa Newsletter</h3>
+                <p className='mb-6 text-gray-300 text-lg'>Receba novidades, dicas e conteúdos exclusivos no seu e-mail.</p>
             </div>
 
-            <div>
+            <div className="mt-12">
                 <form action={`https://formsubmit.co/${email}`} method='POST' className='flex flex-col sm:flex-row items-center gap-2'>
-                    <input type='email' name='email' placeholder='Digite o seu e-mail' required className='flex-1 px-4 py-2 border rounded-lg focus:ring-2 foucs:ring-blue-500 focus:outline_none w-full'/>
-                    <button type='submit' className='bg-orange-400 text-white font-semibold p-2 rounded-lg hover:bg-blue-700 transition w-full sm:w-auto'>Inscrever-se</button>
+                    <input type='email' name='email' placeholder='Digite o seu e-mail' required className='flex-1 px-2 py-3 border rounded-lg w-full'/>
+                    <button type='submit' className='bg-cdh-orange mt-2 text-white font-semibold p-3 rounded-lg hover:bg-blue-700 transition w-full sm:w-auto'>Inscrever-se</button>
                 </form>
 
-                <p className='text-xs mt-4 text-gray-600'>Prometemos não enviar spam!</p>
+                <p className='text-xs mt-4 text-gray-300'>Prometemos não enviar spam!</p>
             </div>
         </div>
     );
