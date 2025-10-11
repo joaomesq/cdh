23f56e082aefd335f53b2ba8bce19a098e565caa
João Baptista Mesquita <joaob.mesquita1@gmail.com>
2025-10-10 23:18:00 -0300

    definindo as cards da section fazer-parte

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps{
 
 export default function Card({titulo, descricao, background = ''}: CardProps){
     return(
-        <div className={`flex items-center justify-center hover:shadow flex-col rounded-lg h-[300px] p-4 text-center m-2 ${background !== ''? background: "bg-white border"}`}>
+        <div className={`flex items-center justify-center hover:shadow-lg border flex-col rounded-lg h-[300px] p-4 text-center m-2 ${background !== ''? background: "bg-black bg-opacity-10"} hover:bg-opacity-15`}>
             <h4 className='font-semibold text-2xl text-gray-700 mb-2'>{titulo}</h4>
             <p className="text-gray-400 text-lg mt-6">{descricao}</p>
         </div>
