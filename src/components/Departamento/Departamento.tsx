48066a16233a4d6c40f6cc7b0a9ffe5f272a8817
João Baptista Mesquita <joaob.mesquita1@gmail.com>
2025-10-11 23:23:16 -0300

    estilização da página equipa

--- src/components/Departamento/Departamento.tsx
+++ src/components/Departamento/Departamento.tsx
@@ -2,15 +2,15 @@ interface DepartamentoProps{
     titulo: string;
     descricao: string;
     colorBorder: string;
-    membros: string;
+    membros: int;
 }
 
-export default function Departamento({titulo, descricao, colorBorder, membros = "1"}: DepartamentoProps){
+export default function Departamento({titulo, descricao, colorBorder, membros = 1}: DepartamentoProps){
     return(
-        <section className={`h-[250px] p-4 border border-t-${colorBorder} border-t-8 rounded-lg hover:shadow flex flex-col justify-center`}>
+        <section className={`h-[250px] p-4 border border-t-8 border-t-${colorBorder} rounded-lg hover:shadow flex flex-col justify-center`}>
             <h2 className="text-xl font-bold text-gray-700 mb-4">{titulo}</h2>
             <p className="text-gray-400 mb-4">{descricao}</p>
-            <p className="text-sm text-gray-400">{`${membros} membros`}</p>
+            <p className="text-sm">{`${membros} membros`}</p>
         </section>
     );
 }
