2e4e391a744e787596511498e810cf8d51fe5aae
João Baptista Mesquita <joaob.mesquita1@gmail.com>
2025-10-13 07:58:34 -0300

    corrigindo desenvolvimento para desenvolvedores

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -11,7 +11,7 @@ interface Slide{
 const slides: Slide[] = [
     {
         imagem: "/img/5.jpg",
-        titulo: "Comunidade de desenvolvimento do Huambo",
+        titulo: "Comunidade de desenvolvedores do Huambo",
         descricao: "Promovendo o desenvolvendo tecnológico e a inovação em Angola através da colaboração entre desenvolvedores, capacitação e partilha de conhecimento."
     },
     {
