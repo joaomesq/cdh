a9a2522e40c0e41b96a0821467b97361114acd16
João Mesquita <joaob.mesquita1@gmail.com>
2026-03-22 04:32:36 +0100

    Adicionando Comunidade e Inovação a categorias

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,7 @@ const blog = defineCollection({
   schema: z.object({
     title: z.string(),
     summary: z.string().max(240),
-    category: z.enum(['Evento','Parcerias','Operações','Tecnologia', "Programção"]).default('Tecnologia'),
+    category: z.enum(['Evento', 'Comunidade', 'Inovação', 'Parcerias','Operações','Tecnologia', "Programção"]).default('Tecnologia'),
     cover: z.string().optional(),
     publishedAt: z.string(), // ISO date
     author: z.string().default('Redação CDH'),
